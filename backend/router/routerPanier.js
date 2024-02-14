8e366439213d1758a2b43fe4a0006bfc3a3e3ed1
boukhaddejatas <boukhaddejatas@gmail.com>
2024-02-14 13:06:25 +0100

    backend panier

--- backend/router/routerPanier.js
+++ backend/router/routerPanier.js
@@ -5,7 +5,7 @@ const controllercart = require('../controller/Paniercontroller.js')
 
 RouterCart.get('/getAllcarts/:id',controllercart.getAllcarts)
 RouterCart.delete('/deletecart/:id/:oneProduct',controllercart.deletecart)
-RouterCart.post('/addtoCart/:idu/:idp',controllercart.addtoCart)
+RouterCart.post('/addtoCart/:idu/:idp',controllercart.addtoCart )
 
 
 module.exports= RouterCart
