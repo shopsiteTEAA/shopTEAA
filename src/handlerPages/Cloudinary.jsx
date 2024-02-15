cf324edae9b794b3a7c02b4a52e06f5c67575aae
Mohamedmatri <hammamatri3@gmail.com>
2024-02-15 23:28:35 +0100

    done

--- src/handlerPages/Cloudinary.jsx
+++ src/handlerPages/Cloudinary.jsx
@@ -1,34 +1,60 @@
-import React, { useState } from 'react'
+import React, { useState } from "react";
 
 const Cloudinary = () => {
-  const [file,setFile]=useState("")
+  const [file, setFile] = useState("");
+  const [image, setImage] = useState("");
 
-  const previewFiles= (file)=>{
+  const previewFiles = (file) => {
+    const reader = new FileReader();
+    reader.readAsDataURL(file);
 
-  }
-  const handleChange = (e) =>{    //(e) = event 
+    reader.onloadend = () => {
+      setImage(reader.result);
+      console.log(image);
+    };
+  };
+  const handleChange = (e) => {
+    //(e) = event
     const file = e.target.files[0];
-    setFile(file)          //Now we need to convert this file to something readble in js
+    setFile(file);
+    previewFiles(file); //Now i need to convert this file to something readble in js
     console.log(file);
     // setFile(e.target.files)
-  }  
-  
-  const handleSubmit = () =>{    //(e) = event 
+  };
+
+  const handleSubmit = () => {
+    //(e) = event
     // console.log(e.target.files);
     // setFile(e.target.files)
-  }                   
+  };
 
   return (
     <div>
-      <div className='container mx-auto'>
-<form onSubmit={e=> handleSubmit(e)}>
-<label class="block mb-2 text-sm font-medium text-gray-900 dark:text-white" for="file_input">Upload file</label>
-<input class="block w-full text-sm text-gray-900 border border-gray-300 rounded-lg cursor-pointer bg-gray-50 dark:text-gray-400 focus:outline-none dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400" aria-describedby="file_input_help" id="file_input" type="file" onChange={e=>handleChange(e)} required accept='image/png, image/jpeg, image/jpg, image/jfif'/>
-<button className='mb-6 inline-block w-full rounded bg-red-600 px-6 py-2.5 font-medium uppercase leading-normal text-white hover:shadow-md hover:bg-teal-500'>Submit</button>
-</form>
-</div>
+      <div className="container mx-auto">
+        <form onSubmit={(e) => handleSubmit(e)}>
+          <label
+            class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+            for="file_input"
+          >
+            Upload file
+          </label>
+          <input
+            class="block w-full text-sm text-gray-900 border border-gray-300 rounded-lg cursor-pointer bg-gray-50 dark:text-gray-400 focus:outline-none dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400"
+            aria-describedby="file_input_help"
+            id="file_input"
+            type="file"
+            onChange={(e) => handleChange(e)}
+            required
+            accept="image/png, image/jpeg, image/jpg, image/jfif"
+          />
+          <button className="mb-6 inline-block w-full rounded bg-red-600 px-6 py-2.5 font-medium uppercase leading-normal text-white hover:shadow-md hover:bg-teal-500">
+            Submit
+          </button>
+        </form>
+      </div>
+      <img src={image} alt="" />
     </div>
-  )
-}
+  );
+};
 
-export default Cloudinary
+export default Cloudinary;
