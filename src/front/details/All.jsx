53c760bdf6428e0ce6d672ba52a9a7800857df7d
boukhaddejatas <boukhaddejatas@gmail.com>
2024-02-16 15:59:11 +0100

    Product details

--- src/front/details/All.jsx
+++ src/front/details/All.jsx
@@ -2,11 +2,15 @@ import React from 'react';
 import Description from "./description.jsx";
 import Details from "./details.jsx";
 import RelatedItem from "./relateditem.jsx";
+import Navbar from '../../Ccomponents/Navbar.jsx';
+import Footer from '../../Ccomponents/Fotter.jsx';
+
+
 
 function Productdetails() {
   return (
     <div>
-       
+       <Navbar/>
       <div className="flex w-full h-[800px] justify-center items-center"  style={{justifyContent:'space-between'}}>
       <Details/>
       <Description/>
@@ -15,6 +19,8 @@ function Productdetails() {
       <div className=" flex justify-start mx-50 mt-20">
         <RelatedItem/>
       </div>
+
+      <Footer/>
     </div>
   )
 }
